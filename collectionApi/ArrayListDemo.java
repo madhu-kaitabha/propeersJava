994726e23f183c282854844c8b90896970bddb29
madhu <>
2025-04-20 19:58:03 +0530

    collection frame work Array List 1

---
+++ collectionApi/ArrayListDemo.java
@@ -0,0 +1,10 @@
+package collectionApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List arr = new ArrayList<Integer>();
+    }
+}
